1dabc382e3b72d658b43e69a14a3ccbc591fa01d
Ani-dev <anu991951@gmail.com>
2025-01-30 12:08:57 +0530

    one change

--- src/Components/OurExpertise/OurExpertise.jsx
+++ src/Components/OurExpertise/OurExpertise.jsx
@@ -10,7 +10,7 @@ export const OurExpertise = () => {
         </div>
         <div className="flex items-center justify-center w-full">
           <div className="flex flex-col md:flex-row justify-center text-white">
-          <div className="flex flex-col items-center text-center border border-[#6E6E6E] w-[320px] rounded-[32px] p-10 mt-10 md:m-2 md:p-6">
+          <div className="flex flex-col items-center text-center border border-[#6E6E6E] md:w-1/4 rounded-[32px] p-10 mt-10 md:m-2 md:p-6">
               <div className="flex items-center justify-center p-[20px]">
                 <img className=" w-[100px]" src="/src/assets/html.gif" alt="web-development" />
               </div>
@@ -21,7 +21,7 @@ export const OurExpertise = () => {
                 <p>&quot;Beautiful, functional, and responsive websites designed to meet your needs.&quot;</p>
               </div>
             </div>
-            <div className="flex flex-col items-center text-center border border-[#6E6E6E] w-[320px] rounded-[32px] p-10 mt-10 md:m-2 md:p-6">
+            <div className="flex flex-col items-center text-center border border-[#6E6E6E] md:w-1/4 rounded-[32px] p-10 mt-10 md:m-2 md:p-6">
               <div className="flex items-center justify-center p-[20px]">
               <img className=" w-[100px]" src="/src/assets/mobile-app.png" alt="web-development" />
               </div>
@@ -33,7 +33,7 @@ export const OurExpertise = () => {
               </div>
             </div>
 
-            <div className="flex flex-col items-center text-center border border-[#6E6E6E] w-[320px] rounded-[32px] p-10 mt-10 md:m-2 md:p-6">
+            <div className="flex flex-col items-center text-center border border-[#6E6E6E] md:w-1/4 rounded-[32px] p-10 mt-10 md:m-2 md:p-6">
               <div className="flex items-center justify-center p-[20px]">
               <img className=" w-[100px]" src="/src/assets/custom-software.png" alt="web-development" />
               </div>
@@ -45,7 +45,7 @@ export const OurExpertise = () => {
               </div>
             </div>
 
-            <div className="flex flex-col items-center text-center border border-[#6E6E6E] w-[320px] rounded-[32px] p-10 mt-10 md:m-2 md:p-6">
+            <div className="flex flex-col items-center text-center border border-[#6E6E6E] md:w-1/4 rounded-[32px] p-10 mt-10 md:m-2 md:p-6">
               <div className="flex items-center justify-center p-[20px]">
               <img className=" w-[100px]" src="/src/assets/custom-software.png" alt="web-development" />
               </div>
