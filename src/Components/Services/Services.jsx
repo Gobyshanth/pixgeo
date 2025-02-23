edf9fad24a3ae5991603d45f0ebc48cbabca5293
Ani-dev <anu991951@gmail.com>
2025-02-23 10:41:06 +0530

    all changes

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -15,6 +15,146 @@ export const Services = () => {
           </div>
         </div>
       </section>
+
+      <section className="bg-custom-mdend md:bg-gradient-to-b from-[#0f0f0f] via-[#362134] to-[#181a22]">
+        <div className="flex flex-col items-center justify-center py-[50px] font-courier">
+          <div className="w-[80%]">
+            <div className="flex items-center">
+              <h2 className="text-white md:text-4xl text-3xl font-bold pr-8 md:pb-10 pb-5 pl-[10px]">
+                Web Development
+              </h2>
+            </div>
+            <div className="flex items-center justify-center w-full">
+              <div className="flex flex-col md:flex-row justify-center text-white">
+                <div className="flex flex-col items-center text-center md:w-1/2 p-10 mt-10 md:m-2 md:p-6">
+                  <div className="flex items-center justify-center p-[20px]">
+                    <img
+                      className=""
+                      src="/src/assets/21034578.png"
+                      alt="web-development"
+                    />
+                  </div>
+                  <div className="text-center text-[24px] font-semibold pb-4">
+                    <h3>Custom design & development</h3>
+                  </div>
+                </div>
+                <div className="flex flex-col items-center text-center md:w-1/2 p-10 mt-10 md:m-2 md:p-6">
+                  <div className="flex items-center justify-center p-[20px]">
+                    <img
+                      className=""
+                      src="/src/assets/21034578.png"
+                      alt="web-development"
+                    />
+                  </div>
+                  <div className="text-center text-[24px] font-semibold pb-4">
+                    <h3>SEO - Friendly Architecture</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center justify-center py-[50px] font-courier">
+          <div className="w-[80%]">
+            <div className="flex items-center">
+              <h2 className="text-white md:text-4xl text-3xl font-bold pr-8 md:pb-10 pb-5 pl-[10px]">
+              Mobile App Development
+              </h2>
+            </div>
+            <div className="flex items-center justify-center w-full">
+              <div className="flex flex-col md:flex-row justify-center text-white">
+                <div className="flex flex-col items-center text-center p-10 mt-10 md:m-2 md:p-6">
+                  <div className="flex items-center justify-center p-[20px]">
+                    <img
+                      className="w-[520] h-[auto]"
+                      src="/src/assets/platform-apps.png"
+                      alt="web-development"
+                    />
+                  </div>
+                  <div className="text-center text-[24px] text-minus-3 font-semibold pb-4">
+                    <h3>Native and cross - Platform apps</h3>
+                  </div>
+                </div>
+                <div className="flex flex-col items-center text-center p-10 mt-10 md:m-2 md:p-6">
+                  <div className="flex items-center justify-center p-[20px]">
+                    <img
+                      className="w-[520] h-[auto]"
+                      src="/src/assets/platform-apps.png"
+                      alt="web-development"
+                    />
+                  </div>
+                  <div className="text-center text-[24px] text-minus-3 font-semibold pb-4">
+                    <h3>Scalable, Secure Backends</h3>
+                  </div>
+                </div>
+                <div className="flex flex-col items-center text-center p-10 mt-10 md:m-2 md:p-6">
+                  <div className="flex items-center justify-center p-[20px]">
+                    <img
+                      className="w-[520] h-[auto]"
+                      src="/src/assets/platform-apps.png"
+                      alt="web-development"
+                    />
+                  </div>
+                  <div className="text-center text-[24px] text-minus-3 font-semibold pb-4">
+                    <h3>User-Friendly and Intuitive Interfaces</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center justify-center py-[50px] font-courier">
+          <div className="w-[80%]">
+            <div className="flex items-center">
+              <h2 className="text-white md:text-4xl text-3xl font-bold pr-8 md:pb-10 pb-5 pl-[10px]">
+              Custom Software Development
+              </h2>
+            </div>
+            <div className="flex items-center justify-center w-full">
+              <div className="flex flex-col md:flex-row justify-center text-white">
+                <div className="flex flex-col items-center text-center p-10 mt-10 md:m-2 md:p-6">
+                  <div className="flex items-center justify-center p-[20px]">
+                    <img
+                      className="w-520px"
+                      src="/src/assets/platform-apps.png"
+                      alt="web-development"
+                    />
+                  </div>
+                  <div className="text-center text-[24px] text-minus-3 font-semibold pb-4">
+                    <h3>Scalable enterprise - Grade Solutions</h3>
+                  </div>
+                </div>
+                <div className="flex flex-col items-center text-center p-10 mt-10 md:m-2 md:p-6">
+                  <div className="flex items-center justify-center p-[20px]">
+                    <img
+                      className="w-520px"
+                      src="/src/assets/platform-apps.png"
+                      alt="web-development"
+                    />
+                  </div>
+                  <div className="text-center text-[24px] text-minus-3 font-semibold pb-4">
+                    <h3>Scalable, Secure Backends</h3>
+                  </div>
+                </div>
+                <div className="flex flex-col items-center text-center  p-10 mt-10 md:m-2 md:p-6">
+                  <div className="flex items-center justify-center p-[20px]">
+                    <img
+                      className="w-520px"
+                      src="/src/assets/platform-apps.png"
+                      alt="web-development"
+                    />
+                  </div>
+                  <div className="text-center text-[24px] text-minus-3 font-semibold pb-4">
+                    <h3>Agile development approch</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
